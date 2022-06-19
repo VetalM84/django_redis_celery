ecbddd9877741774789aacc5a495f3979b0f6504
Vitalii Mytenko <vetalmit@gmail.com>
2022-06-19 20:21:21 +0300

    Add recurrent task

--- celery_redis/tasks.py
+++ celery_redis/tasks.py
@@ -10,3 +10,11 @@ def fibon(n):
     for _ in range(n):
         a, b = b, a + b,
     return a
+
+
+@shared_task
+def fetch_weather():
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=5b9dee532cf418833e63baf01e31e197'
+    city = 'London'
+    city_weather = requests.get(url.format(city)).json()
+    return city_weather.get('wind')
