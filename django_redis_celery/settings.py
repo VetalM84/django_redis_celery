f74277f1c47ce4eb62de25810294fe82fe562cef
Vitalii Mytenko <vetalmit@gmail.com>
2022-06-19 21:07:12 +0300

    Add wsgi_celery app

--- django_redis_celery/settings.py
+++ django_redis_celery/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'celery_redis',
+    'wsgi_celery',
     # for rabbitmq
     'django_celery_results',
     # for recurrent tasks
